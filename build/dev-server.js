20fb436f0671e41ab6ca7287344ffde52079c737
methodispost <597463474@qq.com>
2017-08-29 18:44:49 +0800

    删除多余文件、格式化代码、增加资源文件、通过express框架配置URL接口，模拟后台数据

--- build/dev-server.js
+++ build/dev-server.js
@@ -21,6 +21,42 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+/**
+ * 使用express框架启动一个服务器
+ */
+//1.读取数据
+var appData = require('../data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+//2.使用express配置路由，指定接口请求
+var apiRoutes = express.Router();//定义一个路由
+//配置请求路由和响应
+apiRoutes.get('/seller', function (req, res) {
+  res.json({
+    errno: 0,//错误码
+    data: seller
+  })
+});
+
+apiRoutes.get('/goods', function (req, res) {
+  res.json({
+    errno: 0,//错误码
+    data: goods
+  })
+});
+
+apiRoutes.get('/ratings', function (req, res) {
+  res.json({
+    errno: 0,//错误码
+    data: ratings
+  })
+});
+
+app.use('/api', apiRoutes);//暴露API接口
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
